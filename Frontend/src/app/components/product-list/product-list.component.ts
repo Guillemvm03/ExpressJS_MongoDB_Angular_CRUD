077cc0bf87b0a518211c9ee7de80681dc09e1a95
Guillem <guillemvicent33@gmail.com>
2023-09-15 16:22:46 +0000

    product list

---
+++ Frontend/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,69 @@
+// console.log("hola");
+
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+
+export class ProductListComponent {
+  product?: Product[];
+  currentproduct: Product = {};
+  currentIndex = -1;
+  title = '';
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    
+    this.retrieveProduct();
+  }
+
+  retrieveProduct(): void {
+    this.productService.getAll().subscribe({
+      next: (data) => {
+        this.product = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e)
+    });
+  }
+
+  refreshList(): void {
+    this.retrieveProduct();
+    this.currentproduct = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveProduct(product: Product, index: number): void {
+    this.currentproduct = product;
+    this.currentIndex = index;
+  }
+
+  // removeAllTutorials(): void {
+  //   this.tutorialService.deleteAll().subscribe({
+  //     next: (res) => {
+  //       console.log(res);
+  //       this.refreshList();
+  //     },
+  //     error: (e) => console.error(e)
+  //   });
+  // }
+
+  // searchTitle(): void {
+  //   this.currentTutorial = {};
+  //   this.currentIndex = -1;
+
+  //   this.tutorialService.findByTitle(this.title).subscribe({
+  //     next: (data) => {
+  //       this.tutorials = data;
+  //       console.log(data);
+  //     },
+  //     error: (e) => console.error(e)
+  //   });
+  // }
+}
---
+++ Frontend/src/app/models/product.model.ts
@@ -0,0 +1,6 @@
+export class Product {
+  id?: any;
+  product_name?: string;
+  qty?: string;
+  price?: string;
+}
